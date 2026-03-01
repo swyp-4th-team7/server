299d69adee249f8296da03a7132ea5797db0cfae
Yunmi Jeong <108580095+yunmi-dev@users.noreply.github.com>
2026-03-02 02:10:58 +0900

    [Fix/#22] Jackson null 필드 제거하는 설정 삭제 (#23)
    ## 📌 관련 이슈
    - close #22
    
    ## ✨ 변경 사항
    - ApiResponse @JsonInclude(NON_NULL) 제거
    - application.yml jackson non_null 설정 제거
    
    ## 📸 테스트 증명 (필수)
    변경 전 로그아웃 Response body: {"code":20000,"message":"요청이 성공했습니다."}
    변경 후 로그아웃 Response body: {"code":20000,"message":"요청이
    성공했습니다.","data":null}
    
    ## 📚 리뷰어 참고 사항
    - 안드로이드 코틀린에서 data 필드 nullable 처리를 위해 수정
    - 앞으로 data 없는 API도 data: null로 응답하도록 유의하겠습니다. (앱개발자 피드백 반영)

--- src/main/java/com/swyp/server/global/response/ApiResponse.java
+++ src/main/java/com/swyp/server/global/response/ApiResponse.java
@@ -1,10 +1,8 @@
 package com.swyp.server.global.response;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 
 @Getter
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse<T> {
 
     private final int code;
