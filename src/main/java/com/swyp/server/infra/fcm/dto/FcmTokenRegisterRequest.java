d5cf064a554310553a4c5c7c49657a046f726f3b
sehyeon <Eugene8500@gmail.com>
2026-03-03 00:36:45 +0900

    [Fix/#17] FCM 토큰 검증 메시지 추가

--- src/main/java/com/swyp/server/infra/fcm/dto/FcmTokenRegisterRequest.java
+++ src/main/java/com/swyp/server/infra/fcm/dto/FcmTokenRegisterRequest.java
@@ -4,4 +4,6 @@ import com.swyp.server.infra.fcm.entity.Platform;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record FcmTokenRegisterRequest(@NotBlank String token, @NotNull Platform platform) {}
+public record FcmTokenRegisterRequest(
+        @NotBlank(message = "FCM 토큰은 필수입니다.") String token,
+        @NotNull(message = "플랫폼은 필수입니다.") Platform platform) {}
