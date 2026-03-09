047e129ac20b77bbd258b55c131803ad32eee66a
sehyeon <Eugene8500@gmail.com>
2026-03-10 01:56:15 +0900

    [Feat/#34] Todo 리포지토리 정의

---
+++ src/main/java/com/swyp/server/domain/todo/repository/TodoRepository.java
@@ -0,0 +1,13 @@
+package com.swyp.server.domain.todo.repository;
+
+import com.swyp.server.domain.todo.entity.Todo;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+    List<Todo> findAllByUserIDAndTodoDate(Long userId, LocalDate todoDate);
+
+    Optional<Todo> findByIdAndUserId(Long todoId, Long userId);
+}
