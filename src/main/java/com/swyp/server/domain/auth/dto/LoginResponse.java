f8d00bcdc4c0266b1cee2a3b2a6dc685eb3c2ef5
sehyeon <Eugene8500@gmail.com>
2026-03-09 15:08:18 +0900

    [Refactor/#31] 로그인 응답에서 isNewUser를 profileCompleted로 변경

--- src/main/java/com/swyp/server/domain/auth/dto/LoginResponse.java
+++ src/main/java/com/swyp/server/domain/auth/dto/LoginResponse.java
@@ -1,3 +1,3 @@
 package com.swyp.server.domain.auth.dto;
 
-public record LoginResponse(String accessToken, String refreshToken, boolean isNewUser) {}
+public record LoginResponse(String accessToken, String refreshToken, boolean profileCompleted) {}
