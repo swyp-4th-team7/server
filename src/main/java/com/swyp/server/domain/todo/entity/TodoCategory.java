371599e43a44127c91da4c32bffc564cd7509be3
sehyeon <Eugene8500@gmail.com>
2026-03-12 20:46:42 +0900

    [Refactor/#41] Todo 카테고리 라벨 추가 및 네이밍 수정

--- src/main/java/com/swyp/server/domain/todo/entity/TodoCategory.java
+++ src/main/java/com/swyp/server/domain/todo/entity/TodoCategory.java
@@ -1,15 +1,18 @@
 package com.swyp.server.domain.todo.entity;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public enum TodoCategory {
-    STUDY,
-    HOMEWORK,
-    READING,
-    CREATIVITY,
-    HEALTH,
-    ORGANIZATION,
-    FOCUS,
-    HOUSEHOLD
+    STUDY("공부"),
+    HOMEWORK("숙제"),
+    EXERCISE("운동"),
+    CLEANING("정리"),
+    READING("독서"),
+    HOUSEWORK("집안일"),
+    CREATIVE_ACTIVITY("창의활동");
+
+    private final String label;
 }
