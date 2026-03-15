a50afb02031e0f7d1ec7dbf1739697ae03512920
sehyeon <Eugene8500@gmail.com>
2026-03-15 10:43:57 +0900

    [Feat/#46] UserSticker 엔티티 정의

---
+++ src/main/java/com/swyp/server/domain/sticker/entity/UserSticker.java
@@ -0,0 +1,48 @@
+package com.swyp.server.domain.sticker.entity;
+
+import com.swyp.server.domain.user.entity.User;
+import com.swyp.server.global.SoftDeletableEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(
+        name = "user_stickers",
+        uniqueConstraints = {
+            @UniqueConstraint(
+                    name = "uk_user_sticker_user_id_sticker_id",
+                    columnNames = {"user_id", "sticker_id"})
+        })
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserSticker extends SoftDeletableEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "sticker_id", nullable = false)
+    private Sticker sticker;
+
+    @Builder
+    public UserSticker(User user, Sticker sticker) {
+        this.user = user;
+        this.sticker = sticker;
+    }
+}
