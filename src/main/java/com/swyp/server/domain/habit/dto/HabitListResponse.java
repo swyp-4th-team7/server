903d498c9691d02877517217703a98f20c409d2a
guzzang <991012kim@gmail.com>
2026-03-19 15:27:32 +0900

    [fix/#42] 습관 DTO 이름 수정

--- src/main/java/com/swyp/server/domain/habit/dto/HabitListResponse.java
+++ src/main/java/com/swyp/server/domain/habit/dto/HabitListResponse.java
@@ -4,8 +4,8 @@ import com.swyp.server.domain.habit.entity.Habit;
 
 import java.util.List;
 
-public record HabitListResponse(List<ChildHabitResponse> habits) {
+public record HabitListResponse(List<HabitResponse> habits) {
     public static HabitListResponse from(List<Habit> habits){
-        return new HabitListResponse(habits.stream().map(ChildHabitResponse::from).toList());
+        return new HabitListResponse(habits.stream().map(HabitResponse::from).toList());
     }
 }
--- src/main/java/com/swyp/server/domain/habit/dto/HabitResponse.java
+++ src/main/java/com/swyp/server/domain/habit/dto/HabitResponse.java
@@ -3,14 +3,14 @@ package com.swyp.server.domain.habit.dto;
 import com.swyp.server.domain.habit.entity.Habit;
 import com.swyp.server.domain.user.entity.UserType;
 
-public record ChildHabitResponse(
+public record HabitResponse(
         Long habitId, String title, String duration, String reward, boolean isCompleted) {
 
-    public static ChildHabitResponse from(Habit habit){
+    public static HabitResponse from(Habit habit){
 
         String reward = (habit.getUser().getUserType().equals(UserType.PARENT)) ? null : habit.getReward();
 
-        return new ChildHabitResponse(
+        return new HabitResponse(
                 habit.getId(),
                 habit.getTitle(),
                 habit.getDuration().getLabel(),
