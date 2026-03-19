efcff1b8f648d894acf5a9c3aeb09e17b07630e2
guzzang <991012kim@gmail.com>
2026-03-19 16:15:25 +0900

    [fix/#42] EntityGraph 추가

--- src/main/java/com/swyp/server/domain/habit/repository/HabitRepository.java
+++ src/main/java/com/swyp/server/domain/habit/repository/HabitRepository.java
@@ -3,6 +3,7 @@ package com.swyp.server.domain.habit.repository;
 import com.swyp.server.domain.habit.entity.Habit;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,6 @@ import org.springframework.stereotype.Repository;
 public interface HabitRepository extends JpaRepository<Habit, Long> {
     Optional<Habit> findByIdAndUserId(Long habitId, Long userId);
 
+    @EntityGraph(attributePaths = "user")
     List<Habit> findAllByUserIdOrderByIsCompletedAscIdDesc(Long userId);
 }
