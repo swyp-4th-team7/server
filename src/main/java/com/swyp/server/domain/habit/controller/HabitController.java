bb88361bfeef66ae51e82e47f3b35334421466fb
guzzang <991012kim@gmail.com>
2026-03-24 18:56:28 +0900

    [fix/#69] spotless 적용

--- src/main/java/com/swyp/server/domain/habit/controller/HabitController.java
+++ src/main/java/com/swyp/server/domain/habit/controller/HabitController.java
@@ -41,8 +41,7 @@ public class HabitController {
     @Operation(summary = "보상 조회")
     @GetMapping("/rewards")
     public ResponseEntity<ApiResponse<HabitRewardListResponse>> getHabitRewards(
-            @RequestParam RewardStatus status,
-            @AuthenticationPrincipal Long userId) {
+            @RequestParam RewardStatus status, @AuthenticationPrincipal Long userId) {
         HabitRewardListResponse rewards = habitService.getHabitRewards(userId, status);
         return ResponseEntity.ok(ApiResponse.success(rewards));
     }
