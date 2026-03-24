438382009ff832bc67637e8493d7d94a6292bf21
guzzang <991012kim@gmail.com>
2026-03-24 19:17:01 +0900

    [fix/#69] 보상 조회 DTO 변환 로직 수정

--- src/main/java/com/swyp/server/domain/habit/dto/HabitRewardDetailResponse.java
+++ src/main/java/com/swyp/server/domain/habit/dto/HabitRewardDetailResponse.java
@@ -2,17 +2,13 @@ package com.swyp.server.domain.habit.dto;
 
 import com.swyp.server.domain.habit.entity.Habit;
 import com.swyp.server.domain.habit.entity.HabitDuration;
-import com.swyp.server.domain.user.entity.UserType;
 
 public record HabitRewardDetailResponse(
         Long habitId, String title, HabitDuration duration, String reward) {
 
     public static HabitRewardDetailResponse from(Habit habit) {
 
-        String reward =
-                (habit.getUser().getUserType().equals(UserType.PARENT)) ? null : habit.getReward();
-
         return new HabitRewardDetailResponse(
-                habit.getId(), habit.getTitle(), habit.getDuration(), reward);
+                habit.getId(), habit.getTitle(), habit.getDuration(), habit.getReward());
     }
 }
