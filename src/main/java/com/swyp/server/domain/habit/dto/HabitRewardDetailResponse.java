e796fdfb0f574ac4480a7788f28d0cce2ece215b
guzzang <991012kim@gmail.com>
2026-03-24 17:39:29 +0900

    [fix/#69] 보상 상세 조회 DTO 필드 타입 변경

--- src/main/java/com/swyp/server/domain/habit/dto/HabitRewardDetailResponse.java
+++ src/main/java/com/swyp/server/domain/habit/dto/HabitRewardDetailResponse.java
@@ -1,10 +1,11 @@
 package com.swyp.server.domain.habit.dto;
 
 import com.swyp.server.domain.habit.entity.Habit;
+import com.swyp.server.domain.habit.entity.HabitDuration;
 import com.swyp.server.domain.user.entity.UserType;
 
 public record HabitRewardDetailResponse(
-        Long habitId, String title, String duration, String reward) {
+        Long habitId, String title, HabitDuration duration, String reward) {
 
     public static HabitRewardDetailResponse from(Habit habit) {
 
@@ -12,6 +13,6 @@ public record HabitRewardDetailResponse(
                 (habit.getUser().getUserType().equals(UserType.PARENT)) ? null : habit.getReward();
 
         return new HabitRewardDetailResponse(
-                habit.getId(), habit.getTitle(), habit.getDuration().getLabel(), reward);
+                habit.getId(), habit.getTitle(), habit.getDuration(), reward);
     }
 }
