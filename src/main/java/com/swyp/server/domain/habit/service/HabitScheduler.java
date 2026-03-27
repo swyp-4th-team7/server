8cff39d03af6588cd23036d113cbbcc0d3c41456
guzzang <991012kim@gmail.com>
2026-03-27 18:06:05 +0900

    [fix/#77] 불필요한 주석 제거

--- src/main/java/com/swyp/server/domain/habit/service/HabitScheduler.java
+++ src/main/java/com/swyp/server/domain/habit/service/HabitScheduler.java
@@ -23,7 +23,7 @@ public class HabitScheduler {
     }
 
     @Transactional
-    @Scheduled(cron = "0 0 0 * * *") // 매일 자정 0시 0분 0초
+    @Scheduled(cron = "0 0 0 * * *")
     public void resetDailyHabits() {
         log.info("매일 습관 완료 상태 초기화 스케줄러 시작");
         habitRepository.resetAllHabits();
